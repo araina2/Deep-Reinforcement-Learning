dfd407f043113da9a0eccc99dbc4490ec735c909
Anshu Raina <araina@cs.wisc.edu>
2017-04-11 13:08:52 -0500

    Fixes to the LSTM version.

--- drl_lstm.py
+++ drl_lstm.py
@@ -8,7 +8,7 @@ from keras.layers import Dense, LSTM
 from keras.optimizers import RMSprop
 
 EPISODES = 5000
-seq_size = 3
+timesteps = 100
 
 class DQNAgent:
     def __init__(self, state_size, action_size, state):
@@ -26,8 +26,7 @@ class DQNAgent:
     def _build_model(self):
         # Neural Net for Deep-Q learning Model
         model = Sequential()
-        X_train3D = np.zeros((1, seq_size, state_size))
-        #print('The state size is : ', X_train3D.shape)
+        X_train3D = np.zeros((1, timesteps, state_size))
         model.add(LSTM(output_dim=20, activation='tanh', 
                       input_shape=X_train3D.shape[1:]))
         model.add(Dense(20, activation='tanh', init='uniform'))
@@ -48,28 +47,27 @@ class DQNAgent:
     def replay(self, batch_size):
         batch_size = min(batch_size, len(self.memory))
         minibatch = random.sample(self.memory, batch_size)
-        X = np.zeros((batch_size, seq_size, self.state_size))
+        X = np.zeros((batch_size, timesteps, self.state_size))
         Y = np.zeros((batch_size, self.action_size))
+        
         for i in range(batch_size):
             state, action, reward, next_state, done = minibatch[i]
-            state3D = np.zeros((state.shape[0], seq_size, state.shape[1]))
-            next_state3D = np.zeros((state.shape[0], seq_size, state.shape[1]))
-            #print('The state size for predict is : ', state3D.shape)
-            for i in range(state.shape[0]):
-                for j in range(seq_size):
-                    count = 0
+            state3D = np.zeros((batch_size, timesteps, state.shape[1]))
+            next_state3D = np.zeros((batch_size, timesteps, state.shape[1]))
+            for x in range(state.shape[0]):
+                for j in range(timesteps):
                     for k in range(state.shape[1]):
-                        if (state.shape[1] - k - 1) <= j:
-                            state3D[i][j][k] = state[i][count]
-                            count = count + 1
+                        if j >= 1:
+                            state3D[x][j-1] = state3D[x][j]
+                        state3D[x][j][k] = state[x][k]
+
             
-            for i in range(next_state.shape[0]):
-                for j in range(seq_size):
-                    count = 0
+            for x in range(next_state.shape[0]):
+                for j in range(timesteps):
                     for k in range(next_state.shape[1]):
-                        if (next_state.shape[1] - k - 1) <= j:
-                            next_state3D[i][j][k] = state[i][count]
-                            count = count + 1
+                        if j >= 1:
+                            next_state3D[x][j-1] = next_state3D[x][j]
+                        next_state3D[x][j][k] = next_state[x][k]
             
             target = self.model.predict(state3D)[0]
             if done:
@@ -100,28 +98,25 @@ if __name__ == "__main__":
     for e in range(EPISODES):
         state = env.reset()
         state = np.reshape(state, [1, state_size])
-        state3D = np.zeros((state.shape[0], seq_size, state.shape[1]))
-        for time in range(1000):
-            env.render()
-            
-            for i in range(state.shape[0]):
-                for j in range(seq_size):
-                    count = 0
-                    for k in range(state.shape[1]):
-                        if (state.shape[1] - k - 1) <= j:
-                            state3D[i][j][k] = state[i][count]
-                            count = count + 1
-            
-            action = agent.act(state3D)
-            next_state, reward, done, _ = env.step(action)
-            reward = reward if not done else -10
-            next_state = np.reshape(next_state, [1, state_size])
-            agent.remember(state, action, reward, next_state, done)
-            state = next_state
-            if done or time == 999:
-                print("episode: {}/{}, score: {}, e: {:.2}"
-                        .format(e, EPISODES, time, agent.epsilon))
-                break
+        state3D = np.zeros((state.shape[0], timesteps, state.shape[1]))
+        for i in range(state.shape[0]):
+            for time in range(timesteps):
+                env.render()
+                for k in range(state.shape[1]):
+                    if time >= 1:
+                        state3D[i][time-1] = state3D[i][time]
+                    state3D[i][time][k] = state[i][k]
+                
+                action = agent.act(state3D)
+                next_state, reward, done, _ = env.step(action)
+                reward = reward if not done else -10
+                next_state = np.reshape(next_state, [1, state_size])
+                agent.remember(state, action, reward, next_state, done)
+                state = next_state
+                if done or time == 999:
+                    print("episode: {}/{}, score: {}, e: {:.2}"
+                            .format(e, EPISODES, time, agent.epsilon))
+                    break
         agent.replay(32)
         # if e % 10 == 0:
             # agent.save("./save/cartpole.h5") 
